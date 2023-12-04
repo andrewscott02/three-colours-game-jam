fc379c662d3e06480d6d5eb789b1cbd4101af9c0
Andrew Scott <as243879@falmouth.ac.uk>
2023-12-04 12:29:18 +0000

    Player -Setting up impulses

--- Assets/Player/PlayerSoundwaveManager.cs
+++ Assets/Player/PlayerSoundwaveManager.cs
@@ -6,26 +6,28 @@ public class PlayerSoundwaveManager : MonoBehaviour
 {
     public GameObject soundWaveGO;
 
-    public Vector2 breathingMinMax = new Vector2(0.5f, 1);
+    public Vector2 baseSoundwaveMinMax = new Vector2(1.4f, 1.8f);
+    Vector2 soundwaveMinMax = new Vector2(1.4f, 1.8f);
     public float stopDelay = 0.4f;
-    public float tInterval = 0.1f;
+    public float tInterval = 0.0005f;
     float currentTTime;
     bool advancing = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        soundwaveMinMax = baseSoundwaveMinMax;
         advancing = true;
         currentTTime = 0;
-        UpdateSoundWaveSize(breathingMinMax.x);
+        UpdateSoundWaveSize(soundwaveMinMax.x);
     }
 
     // Update is called once per frame
     void Update()
     {
         currentTTime = Mathf.Clamp(advancing ? currentTTime + tInterval : currentTTime - tInterval, 0, 1);
-        float size = Remap(currentTTime, 0, 1, breathingMinMax.x, breathingMinMax.y);
-        UpdateSoundWaveSize(size);
+        float size = Remap(currentTTime, 0, 1, soundwaveMinMax.x, soundwaveMinMax.y);
+        UpdateSoundWaveSize(size + tempImpulse);
 
         if (currentTTime >= 1)
         {
@@ -34,6 +36,22 @@ public class PlayerSoundwaveManager : MonoBehaviour
         else if (currentTTime <= 0)
         {
             advancing = true;
+        }
+
+        if (resettingSoundwave)
+        {
+            returnT = Mathf.Clamp(returnT + returnInterval, 0, 1);
+            tempImpulse = Mathf.Lerp(tempImpulse, 0, returnT);
+
+            if (returnT >= 1)
+            {
+                resettingSoundwave = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Impulse(2.5f, 0.001f);
         }
     }
 
@@ -47,5 +65,18 @@ public class PlayerSoundwaveManager : MonoBehaviour
         float i = (((inputValue - fromMin) / (fromMax - fromMin)) * (toMax - toMin) + toMin);
         i = Mathf.Clamp(i, toMin, toMax);
         return i;
+    }
+
+    float tempImpulse = 0;
+    float returnT = 0;
+    float returnInterval;
+    bool resettingSoundwave = false;
+
+    void Impulse(float strength, float returnInterval)
+    {
+        tempImpulse = strength;
+        resettingSoundwave = true;
+        this.returnInterval = returnInterval;
+        returnT = 0;
     }
 }
