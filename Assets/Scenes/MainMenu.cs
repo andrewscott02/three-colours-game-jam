e056357e525ab3c4574d66318dad9fce55a2241c
Andrew Scott <as243879@falmouth.ac.uk>
2023-12-06 12:35:12 +0000

    Web Build

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -18,10 +18,16 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(scenes[sceneIndex].ToString());
     }
 
+    #if UNITY_WEBPLAYER
+    public static string webplayerQuitURL = "http://google.com";
+    #endif
+
     void CloseApplication()
     {
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+        Application.OpenURL(webplayerQuitURL);
         #else
         Application.Quit();
         #endif
